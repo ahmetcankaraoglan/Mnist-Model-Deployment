50cf1a9ed7de778d8563512e2b3cfa1902af267b
Rajtilak Pal <rajtilakls2510@gmail.com>
2020-06-04 13:42:03 +0530

    Migrated from tensorflow to tensorflowjs

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from flask_cors import CORS, cross_origin
 import base64
 import io
 from PIL import Image
 import numpy as np
-from tensorflow.keras.utils import normalize
-from tensorflow.keras.models import load_model
+import json
+
+
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -26,13 +27,11 @@ def send_image():
     img = Image.open(buf)
     img = img.resize((28, 28), Image.ANTIALIAS)
     image_data = np.array(list(img.getdata()))[:, -1].reshape(28, 28)
-    image_data = normalize(image_data, axis=1)
+    image_data = image_data/max(image_data.ravel())
+
     image_data = image_data.reshape(1, 28, 28, 1)
+    return json.dumps({"image": image_data.tolist()})
 
-    model = load_model('Best_9927.h5')
-    pred = np.argmax(model.predict(image_data))
-    print("Prediction: ", pred)
-    return str(pred)
-
-
+if __name__=='__main__':
+    app.run(debug=True,port=4000)
 
